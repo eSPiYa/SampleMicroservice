449636a834965eaa8fe6f6c5593ba4a96f498b6d
Virgilio Dolosa <vdolosa@live.com.ph>
2024-01-08 20:18:33 +0800

    Added console logging

--- SampleMicroservice.Worker/Program.cs
+++ SampleMicroservice.Worker/Program.cs
@@ -2,6 +2,8 @@ using SampleMicroservice.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Logging.AddConsole();
 
 var host = builder.Build();
 host.Run();
+
--- SampleMicroservice.Worker/Worker.cs
+++ SampleMicroservice.Worker/Worker.cs
@@ -11,11 +11,13 @@ namespace SampleMicroservice.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("Instance '{instance}' started at: {time}", System.Environment.MachineName, DateTimeOffset.Now);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
                 await Task.Delay(1000, stoppingToken);
             }
